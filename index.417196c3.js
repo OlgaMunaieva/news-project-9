a0859722216772dec1d6493df99ae95252e80f5e
OlgaMunaieva <111134698+OlgaMunaieva@users.noreply.github.com>
2023-03-02 11:45:54 +0000

    Deploying to gh-pages from @ OlgaMunaieva/news-project-9@0fbea1b51131d9f058d7377e0d3fb6baf6fcae61 🚀

---
+++ index.417196c3.js
@@ -0,0 +1,2 @@
+var themeSwitcher=document.getElementById("theme-switch");function clickHandler(){this.checked?(document.body.classList.add("light"),document.body.classList.remove("dark"),localStorage.setItem("theme","light")):(document.body.classList.remove("light"),document.body.classList.add("dark"),localStorage.setItem("theme","dark"))}themeSwitcher.checked=!1,themeSwitcher.addEventListener("click",clickHandler);
+//# sourceMappingURL=index.417196c3.js.map
