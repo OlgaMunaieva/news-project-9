5b7fb09288d5b0c0cf45308a588716c7d18f58a8
OlgaMunaieva <111134698+OlgaMunaieva@users.noreply.github.com>
2023-03-04 12:38:48 +0000

    Deploying to gh-pages from @ OlgaMunaieva/news-project-9@6459246b9045e12a5cc25610e45bfc42f8b9b127 🚀

---
+++ calendar.fae20c7b.js
@@ -0,0 +1,2 @@
+const o=document.querySelector(".prev");console.log(o);const e=document.querySelector(".con-calendar");console.log(e);new Date;console.log("Я тут, ау!");
+//# sourceMappingURL=calendar.fae20c7b.js.map
